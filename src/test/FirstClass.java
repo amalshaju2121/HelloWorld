9537795a89ffa97808ef4749133851caa9ebd574
Amal shaju <amal.shaju@gmail.com>
2021-02-11 04:22:17 +0000

    First Commit

---
+++ src/test/FirstClass.java
@@ -0,0 +1,15 @@
+package test;
+
+public class FirstClass {
+
+	public FirstClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
